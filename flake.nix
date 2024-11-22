e913661595c5fce122e35a21302707549d0010e7
TonyGrif <TonyGriffin2000@gmail.com>
2024-11-21 19:12:57 -0500

    chore: remove unused formatter

--- flake.nix
+++ flake.nix
@@ -14,17 +14,7 @@
     ...
   } @ inputs: let
     inherit (self) outputs;
-    systems = [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
-
     # Reusable nixos modules you might want to export
     # These are usually stuff you would upstream into nixpkgs
     nixosModules = import ./modules/nixos;
