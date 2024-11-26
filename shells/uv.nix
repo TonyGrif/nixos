c62a3043fdd3db3b127e177364b03ac69646afa0
TonyGrif <TonyGriffin2000@gmail.com>
2024-11-25 22:00:59 -0500

    shells: add nix-shell used for uv based projects

---
+++ shells/uv.nix
@@ -0,0 +1,11 @@
+let
+  unstableTarball = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
+  pkgs = import <nixpkgs> {};
+  unstable = import unstableTarball {};
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+      unstable.uv
+  ];
+}
