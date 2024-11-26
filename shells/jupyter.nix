e2c2950d02e903a8d83a49c8720719aa2903238b
TonyGrif <TonyGriffin2000@gmail.com>
2024-11-26 13:52:06 -0500

    shells: add starter nix-shell for jupyter projects

---
+++ shells/jupyter.nix
@@ -0,0 +1,13 @@
+let
+  unstableTarball = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
+  pkgs = import <nixpkgs> {};
+  unstable = import unstableTarball {};
+in
+
+pkgs.mkShellNoCC {
+    packages = with pkgs; [
+      python312Packages.jupyterlab
+      python312Packages.pandas
+      python312Packages.seaborn
+    ];
+}
