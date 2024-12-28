8867fcfa822bb8e3d0ba9bd7d2a1c00f257e6c0f
TonyGrif <TonyGriffin2000@gmail.com>
2024-12-28 10:12:59 -0500

    mod(nix): add xwayland and accelerated graphics to hyprland module

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -16,10 +16,20 @@
       ];
     };
 
-    programs.hyprland.enable = true;
+    programs.hyprland = {
+      enable = true;
+      xwayland.enable = true;
+    };
+    environment.sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+    };
+    hardware = {
+      graphics.enable = true;
+    };
 
     environment.systemPackages = with pkgs; [
       kitty
+      waybar
     ];
   };
 }
