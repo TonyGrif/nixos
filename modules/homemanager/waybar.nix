f057a4c67a13c5c4019cbf8724805f1c32f274f5
TonyGrif <TonyGriffin2000@gmail.com>
2025-01-01 19:13:51 -0500

    chore: formatting

--- modules/homemanager/waybar.nix
+++ modules/homemanager/waybar.nix
@@ -7,17 +7,22 @@
   cfg = config.waybar;
 in {
   options = {
-    waybar= {
+    waybar = {
       enable = lib.mkEnableOption "Enable waybar";
       experimental = lib.mkEnableOption "Enable experimental features when compiling";
     };
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = if cfg.experimental then [(
-        pkgs.waybar.overrideAttrs (oldAttrs: {
-          mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
-        })
-      )] else [pkgs.waybar];
+    home.packages =
+      if cfg.experimental
+      then [
+        (
+          pkgs.waybar.overrideAttrs (oldAttrs: {
+            mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+          })
+        )
+      ]
+      else [pkgs.waybar];
   };
 }
--- modules/nixos/hyprland/default.nix
+++ modules/nixos/hyprland/default.nix
@@ -6,7 +6,7 @@
   ...
 }: let
   cfg = config.hyprland;
-in{
+in {
   imports = [
     ./dolphin.nix
     ./wofi.nix
