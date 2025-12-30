02df7c22ba1919e2bdeadcd16270ab62895d2f6c
TonyGrif <TonyGriffin2000@gmail.com>
2025-12-30 00:08:47 -0500

    mod(hm): remove redundant modules

--- modules/homemanager/default.nix
+++ modules/homemanager/default.nix
@@ -2,8 +2,6 @@
   browsers = import ./browsers.nix;
 
   gnome = import ./gnome.nix;
-  creative = import ./creative;
   coding = import ./coding;
-  office = import ./office;
   hyprland = import ./hyprland.nix;
 }
